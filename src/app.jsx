6d479bb8173abda88eecb92a03ec8c0af32f8da9
xumadeline <madelinemxu@gmail.com>
2026-03-25 14:40:55 -0600

    leaderboard css

--- src/app.jsx
+++ src/app.jsx
@@ -17,10 +17,23 @@ function Header() {
   );
 }
 
+function Footer() {
+  return (
+    <footer className="bg-dark text-white-50">
+      <div className="container-fluid d-flex justify-content-between">
+        <span className="text-reset">Madeline Xu</span>
+        <a className="text-reset" href="https://github.com/madelinecharles/startup" target="_blank">GitHub</a>
+      </div>
+    </footer>
+  );
+}
+
 export default function App() {
   return (
     <div className='body bg-dark text-light'>
       <Header />
+      <main>App will display here</main>
+      <Footer />
     </div>
   );
 }
