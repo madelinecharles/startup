c4679e824e97fe475f84c26f8eb4e8c85b0d617e
xumadeline <madelinemxu@gmail.com>
2026-03-25 14:21:13 -0600

    header

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+function Header() {
+  return (
+    <header>
+      <nav className="navbar fixed-top navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">💧 Drinkly</a>
+        <menu className="navbar-nav">
+          <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+          <li className="nav-item"><a className="nav-link" href="/dashboard">Dashboard</a></li>
+          <li className="nav-item"><a className="nav-link" href="/leaderboard">Leaderboard</a></li>
+          <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+        </menu>
+      </nav>
+    </header>
+  );
+}
+
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body bg-dark text-light'>
+      <Header />
+    </div>
+  );
 }
