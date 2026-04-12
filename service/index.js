9d826b866bb6ac702330d6b2315b210ff7fe8386
xumadeline <madelinemxu@gmail.com>
2026-04-11 19:11:52 -0600

    port 4000

--- service/index.js
+++ service/index.js
@@ -10,7 +10,7 @@ const authCookieName = 'token';
 let users = [];
 let playerData = {}; // keyed by name: { streak, intake, weeklyTotal, lastDate, weekStart }
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
 app.use(cookieParser());
