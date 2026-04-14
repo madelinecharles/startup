2c6948f1df04b51280208e46f2991a67ca851976
xumadeline <madelinemxu@gmail.com>
2026-04-13 22:39:52 -0600

    Remove peer proxy comments

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -2,7 +2,6 @@ const { WebSocketServer, WebSocket } = require('ws');
 const uuid = require('uuid');
 
 function DrinkProxy(httpServer) {
-  // Attach WebSocket server to the existing Express HTTP server
   const socketServer = new WebSocketServer({ server: httpServer });
 
   let connections = [];
@@ -11,7 +10,6 @@ function DrinkProxy(httpServer) {
     const connection = { id: uuid.v4(), alive: true, ws: socket };
     connections.push(connection);
 
-    // Forward messages to everyone except the sender
     socket.on('message', function message(data) {
       connections.forEach((c) => {
         if (c.id !== connection.id && c.ws.readyState === WebSocket.OPEN) {
@@ -20,18 +18,15 @@ function DrinkProxy(httpServer) {
       });
     });
 
-    // Remove connection when client disconnects
     socket.on('close', () => {
       connections = connections.filter((c) => c.id !== connection.id);
     });
 
-    // Mark connection alive when pong is received
     socket.on('pong', () => {
       connection.alive = true;
     });
   });
 
-  // Ping all clients every 10 seconds, terminate any that don't respond
   setInterval(() => {
     connections.forEach((c) => {
       if (!c.alive) return c.ws.terminate();
